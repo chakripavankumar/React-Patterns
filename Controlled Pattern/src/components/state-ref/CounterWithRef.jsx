2330683c690370bb8d03f232a00dc28782a5a4fe
Pavan Kumar <siriyalapavankumar@gmail.com>
2025-12-19 08:26:11 +0530

    Pattern 01 : container  TASK✅

---
+++ Controlled Pattern/src/components/state-ref/CounterWithRef.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { useRef } from "react"
+
+
+const CounterWithRef = () => {
+
+    const countRef = useRef(0); // persisits between re-renders
+    const [ renderCount, setRenderCountref] = useState() // for forceing rerenders
+
+
+    const increment = () {
+       countRef.current = countRef.current + 1;
+         console.log("Ref Count:", countRef.current); 
+    }
+
+  return (
+    <div className="flex justify-around w-full">
+      <div>
+        <h2 className=" text-2xl">  Ref Count: {countRef.current}</h2>
+     <button className="bg-purple-500 text-white p-1 " onClick={increment}>Increment Ref Count</button>
+      </div>
+     <div>
+        
+     </div>
+
+    </div>
+  )
+}
+
+export default CounterWithRef
